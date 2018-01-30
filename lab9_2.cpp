8d007a35d0142ccf005bcfb556524bc0a0e3a88f
Unknown <nichapat18102541@gmail.com>
2018-01-30 11:04:31 +0700

    เสร็จแล้ว ข้อที่ 2 (ทำผิดข้อ)

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,11 +1,24 @@
-#include <iostream>
-
+ #include <iostream>
+#include <fstream>
 using namespace std;
-
-int main() {
     /* เขียนโปรแกรมรับค่ารัศมีวงกลมจากคีย์บอร์ด และหาพื้นที่ของวงกลมเป็นทศนิยม 4 ตำแหน่ง
     โดยกำหนดให้โปรแกรมหาพื้นที่ของวงกลม จนกว่าผู้ใช้งานต้องการออกจากการทำงานของโปรแกรม
     และกำหนดให้แบ่งการทำงานเป็นโปรแกรมย่อยอย่างน้อย 1 ฟังก์ชัน
     */
+      float cArea (float r){
+        float area;
+        area = 3.1416*r*r;
+
+        return area;}
+
+int main() {
+    float r, cmd;
+    do {
+        cin >> cmd;
+        if (cmd == 1) {
+            cin >> r;
+            cout << cArea (r) <<endl;
+        } else {break;}
+        } while(1);
     return 0;
 }
