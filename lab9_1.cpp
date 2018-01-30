1de4311dc4357911c5cf03ab2e2071e565962aab
Unknown <nichapat18102541@gmail.com>
2018-01-30 11:04:21 +0700

    เสร็จแล้ว ข้อที่ 1 (ทำผิดข้อ)

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
-
 using namespace std;
 
+void swap (int * a, int * b) {
+        int Nicha;
+        Nicha = * a;
+        * a = * b;
+        * b = Nicha;
+    }
 int main() {
+        /* code */
+    int  a,b,cmd;
     /*เขียนโปรแกรมรับค่าจำนวนเต็มบวกจากคีย์บอร์ด 2 จำนวน เก็บไว้ในตัวแปร และสลับค่าข้อมูลระหว่างตัวแปรสองตัวแปรดังกล่าว
     โดยกำหนดให้โปรแกรมสลับค่าข้อมูลจนกว่าผู้ใช้งานต้องการออกจากการทำงานของโปรแกรม
     และแบ่งการทำงานของโปรแกรมเป็นโปรแกรมย่อย
     */
-
+ cin >> a;
+    cin >> b;
+    do{
+        cout << "menu\n1.swap()\n2.quit()"<< endl;
+        cin >> cmd;
+        if (cmd==1){
+            swap (&a,&b);
+            cout << a << b;
+        }else {break;}
+    }while(1);
 
 
     return 0;
